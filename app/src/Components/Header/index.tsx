1d87e712f1455377d1323a5c5afd3c7bfcdacf41
João vitor V <resetgamer1@gmail.com>
2021-03-05 04:44:17 -0300

    show username on header

--- app/src/Components/Header/index.tsx
+++ app/src/Components/Header/index.tsx
@@ -3,6 +3,8 @@ import HeaderTabs from '../HeaderTabs'
 
 import { FaSearch, FaUser } from 'react-icons/fa'
 
+import { useAuth } from '../../hooks/auth'
+
 import {
   Container,
   HeaderInfo,
@@ -12,11 +14,13 @@ import {
 import Colors from '../../styles/theme'
 
 const Header: React.FC = () => {
+  const { user } = useAuth()
+
   return (
     <Container>
       <HeaderInfo>
         <HeaderTitle>
-          <h2>Calls</h2>
+          <h2>{user.username}</h2>
         </HeaderTitle>
 
         <HeaderActions>
